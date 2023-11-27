3c0b7c2faafe9479a0f4dd46764e00f976c5eaff
mryeminthu <dhammaarchive@hotmail.com>
2023-11-27 20:56:22 +0630

    Add foreign keys

--- db/migrate/20231127122604_like.rb
+++ db/migrate/20231127122604_like.rb
@@ -1,8 +1,8 @@
 class Like < ActiveRecord::Migration[7.1]
   def change
     create_table :likes do |t|
-      t.integer :user_id
-      t.integer :post_id
+      t.references :user, null: false, foreign_key: {to_table: 'users'}, index: true
+      t.references :post, null: false, foreign_key: {to_table: 'posts'}, index: true
       t.timestamps
     end
   end
--- db/migrate/20231127122759_comment.rb
+++ db/migrate/20231127122759_comment.rb
@@ -1,8 +1,8 @@
 class Comment < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :post_id
+      t.references :user, null: false, foreign_key: { to_table: 'users' }, index: true
+      t.references :post, null: false, foreign_key: { to_table: 'posts' }, index: true
       t.text :text
       t.timestamps
     end
