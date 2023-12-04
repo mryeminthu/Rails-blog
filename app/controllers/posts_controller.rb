e8b858f37813f3e8eb1bbb133d0736394bf4b5c8
mryeminthu <dhammaarchive@hotmail.com>
2023-12-04 11:45:45 +0630

    Add show method in posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,11 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @posts = @user.posts
   end
+
+  def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+  end
       
 end
   
