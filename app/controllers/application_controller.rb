24f9e8c74a82264a683b8bd74f7f1269a26b3cbd
May Pyone <maypyone015@gmail.com>
2023-12-21 18:25:47 +0630

    Add configuration for sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:name, :email)
+    end
   end
 end
