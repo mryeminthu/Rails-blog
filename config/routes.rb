d17e6159fd1d1b9ec0e07d096798217f07cc94c9
mryeminthu <dhammaarchive@hotmail.com>
2023-12-22 16:51:55 +0630

    Add api endpoints for posts and create comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :users do
+      resources :posts do
+        resources :comments
+      end
+    end
+  end
   devise_for :users
 
   devise_scope :user do
